a550a514a50b34d07a9438cc41c410e298011094
Ahmet <rganned@gmail.com>
2022-12-15 17:02:30 +0300

    add poke states in poke class

--- src/pages/overlay/[id].tsx
+++ src/pages/overlay/[id].tsx
@@ -1,24 +1,42 @@
 import { poke } from "@/utils/poke";
 import { tmiClient } from "@/utils/tmi";
 import { GetServerSideProps } from "next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function GameOverlay({ id }: { id: string }) {
+  const [pokeState, setPokeState] = useState<{
+    hp: number;
+    name: string | null;
+  }>({
+    hp: 0,
+    name: null,
+  });
+
   // connect twitch irc server with tmijs
   tmiClient.connect();
 
-  useEffect(() => {
+  /* useEffect(() => {
     return () => {
       tmiClient.disconnect();
       tmiClient.removeAllListeners();
     };
+  }, []); */
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPokeState({ ...pokeState, hp: poke.hp, name: poke.name });
+    }, 3000);
+
+    return () => clearInterval(interval);
   }, []);
 
   // if connected to irc server
   tmiClient
     .on("connected", (address) => {
       console.log(`tmi: connected to irc server(${address})`);
-      tmiClient.join(id);
+      tmiClient.join(id).then(() => {
+        poke.initialize();
+      });
     })
     .on("disconnected", () => {
       console.log("tmi: disconnected to irc server");
@@ -29,13 +47,16 @@ export default function GameOverlay({ id }: { id: string }) {
       const cmd = message.slice(1).split(" ").pop()?.toLowerCase();
       const user = tags.username as string;
 
+      // commands
       if (cmd === "welcomepack") {
         console.log(user, cmd);
         return await poke.welcomePack(user, channel);
+      } else if (cmd === "attack") {
+        return poke.attack(user, channel);
       }
     });
 
-  return <div></div>;
+  return <div>{JSON.stringify(pokeState)}</div>;
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
--- src/utils/poke.ts
+++ src/utils/poke.ts
@@ -2,6 +2,14 @@ import { pokes } from "@/storage/data";
 import supabase from "@/utils/supabase";
 
 class Poke {
+  hp: number;
+  name: string | null;
+
+  constructor() {
+    this.hp = 50;
+    this.name = null;
+  }
+
   randPoke = () => {
     const index = Math.floor(Math.random() * pokes.length - 1);
     const pickPoke = pokes[index];
@@ -23,6 +31,36 @@ class Poke {
       channel: channel,
       poke: randPoke,
     });
+  };
+
+  initialize = () => {
+    this.hp = 50;
+    this.name = this.randPoke();
+  };
+
+  attack = async (user: string, channel: string) => {
+    const attack = Math.floor(Math.random() * 10) + 5;
+    this.hp = this.hp -= attack;
+    console.log(
+      `poke: attacking to poke -> poke: ${this.hp} - user: ${user} - attack: ${attack} - channel: ${channel}`
+    );
+
+    if (this.hp <= 0) {
+      const newPoke = this.randPoke();
+
+      console.log(
+        `poke: cathed to poke -> poke: ${this.name} - user: ${user} - channel: ${channel}`
+      );
+
+      await supabase.from("collections").insert({
+        user: user,
+        channel: channel,
+        poke: newPoke,
+      });
+
+      this.hp = 50;
+      this.name = newPoke;
+    }
   };
 }
 

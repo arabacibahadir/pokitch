0c6d0ff1595cc48cae071f3b1523135fc1f47c71
Ahmet <rganned@gmail.com>
2022-12-17 13:19:31 +0300

    fix import path

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import React from "react";
-import Footer from "./Footer";
 
 type Props = {
   children: React.ReactNode;

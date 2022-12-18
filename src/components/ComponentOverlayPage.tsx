a95e6ee76be32134eb8b6689ebdfffbcb7896a73
Ahmet <rganned@gmail.com>
2022-12-18 17:07:33 +0300

    add delay in overlay update function

--- src/components/ComponentOverlayPage.tsx
+++ src/components/ComponentOverlayPage.tsx
@@ -23,15 +23,17 @@ export default function ComponentOverlayPage({ channel }: Props) {
   });
 
   const getCurrentPoke = async (channel: string) => {
-    await new Promise<void>(() => setTimeout(async () => {
-      const {data} = await supabase
+    await new Promise<void>(() =>
+      setTimeout(async () => {
+        const { data } = await supabase
           .from("active_pokes")
           .select()
           .eq("channel", channel)
           .single();
-      if (!data) return null;
-      setPokeState({...pokeState, health: data.health, name: data.poke});
-    }, 1000));
+        if (!data) return null;
+        setPokeState({ ...pokeState, health: data.health, name: data.poke });
+      }, 1000)
+    );
   };
 
   useEffect(() => {

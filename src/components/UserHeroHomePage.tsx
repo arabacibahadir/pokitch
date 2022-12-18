d1ef3c93e35d684f8a71c966c0be6badcaa4d9a9
Ahmet <rganned@gmail.com>
2022-12-18 14:33:51 +0300

    fix class

--- src/components/UserHeroHomePage.tsx
+++ src/components/UserHeroHomePage.tsx
@@ -1,7 +1,6 @@
 import Button from "@/components/ui/Button";
 import Heading from "@/components/ui/Heading";
 import Link from "@/components/ui/Link";
-import { cx } from "class-variance-authority";
 import { useState } from "react";
 import { BiShow } from "react-icons/bi";
 import { FaClipboardCheck } from "react-icons/fa";
@@ -24,12 +23,14 @@ export default function GuestHeroHomePage({ data }: { data: any }) {
 
   return (
     <div className="space-y-3 rounded-md bg-green-400/25 p-6 shadow">
-      <Heading variant="h3">Welcome {data.channel}!</Heading>
-      <p>
-        Your Pokitch Overlay was successfully generated. Please{" "}
-        <Link href="#how-to-use">click here</Link> to follow the instructions.
-      </p>
-      <div className="inline-flex flex-row gap-2">
+      <div className="space-y-3">
+        <Heading variant="h3">Welcome {data.channel}!</Heading>
+        <p>
+          Your Pokitch Overlay was successfully generated. Please{" "}
+          <Link href="#how-to-use">click here</Link> to follow the instructions.
+        </p>
+      </div>
+      <div className="flex flex-row items-center justify-center gap-2">
         <Button
           variant="success"
           startIcon={<FaClipboardCheck />}
@@ -45,14 +46,15 @@ export default function GuestHeroHomePage({ data }: { data: any }) {
           Show URL
         </Button>
       </div>
-      <div
-        className={cx(
-          "inline-flex select-all rounded-md bg-black bg-opacity-25 p-2 transition-display",
-          showURL ? "block" : "hidden"
-        )}
-      >
-        <p onClick={() => copyURL(data.id)}>{url}</p>
-      </div>
+      {showURL ? (
+        <div
+          className={
+            "inline-flex select-all rounded-md bg-black bg-opacity-25 p-2 transition-display"
+          }
+        >
+          <p>{url}</p>
+        </div>
+      ) : null}
     </div>
   );
 }

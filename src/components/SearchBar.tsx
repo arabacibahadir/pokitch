4b8a879f1ce06438a389b9efbc77c8d3c8f1d534
arabacibahadir <bahadirarabaci@yandex.com>
2022-12-25 14:50:00 +0300

    add user search functionality

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,27 @@
 import Input from "@/ui/Input";
+import {useRouter} from "next/router";
+import {useState} from "react";
 
 type Props = {};
 
 export default function SearchBar({}: Props) {
+    const router = useRouter();
+    const [searchValue, setSearchValue] = useState<string | null>(null);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!searchValue) return;
+        router.push({
+            pathname: "/collections",
+            query: { user: searchValue }
+        });
+        setSearchValue("");
+    };
+
   return (
     <div className="flex items-center justify-center ">
-      <form className="w-full tablet:max-w-xs">
-        <Input placeholder="Search for users..." />
+      <form className="w-full tablet:max-w-xs" onSubmit={handleSubmit}>
+        <Input placeholder="Search for users..." value={searchValue ?? ""} onChange={(e)=>setSearchValue(e.target.value)}/>
       </form>
     </div>
   );

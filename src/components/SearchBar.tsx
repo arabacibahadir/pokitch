89c06bfe86e044ec74e7abb581e775aa8b47619b
arabacibahadir <bahadirarabaci@yandex.com>
2023-01-04 16:00:56 +0300

    add search by channel feature for inventory page
    Co-Authored-By: durmusrasit <12167194+durmusrasit@users.noreply.github.com>

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 import Input from "@/ui/Input";
 import {useRouter} from "next/router";
 import {useState} from "react";
+import {FiRefreshCcw} from "react-icons/Fi";
 
 type Props = {};
 
@@ -8,7 +9,9 @@ export default function SearchBar({}: Props) {
     const router = useRouter();
     const [searchValue, setSearchValue] = useState<string | null>(null);
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const [userSearch, setUserSearch] = useState<boolean | null>(true);
+
+    const handleUserSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (!searchValue) return;
         router.push({
@@ -17,12 +20,28 @@ export default function SearchBar({}: Props) {
         });
         setSearchValue("");
     };
+    const handleChannelSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!searchValue) return;
+        router.push({
+            pathname: "/collections",
+            query: { channel: searchValue }
+        });
+        setSearchValue("");
+    };
 
   return (
     <div className="flex items-center justify-center ">
-      <form className="w-full tablet:max-w-xs" onSubmit={handleSubmit}>
-        <Input placeholder="Search for users..." value={searchValue ?? ""} onChange={(e)=>setSearchValue(e.target.value)}/>
-      </form>
+        <button className="flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-neutral-900/25 hover:bg-neutral-900/50 m-3"
+                onClick={()=>setUserSearch(!userSearch)}>
+        User<FiRefreshCcw/>Channel
+        </button>
+
+        {userSearch ?<form className="w-full tablet:max-w-xs" onSubmit={handleUserSearchSubmit}>
+        <Input placeholder= "Search for users..." value={searchValue ?? ""} onChange={(e)=>setSearchValue(e.target.value)}/>
+      </form>:<form className="w-full tablet:max-w-xs" onSubmit={handleChannelSearchSubmit}>
+            <Input placeholder= "Search for channels..." value={searchValue ?? ""} onChange={(e)=>setSearchValue(e.target.value)}/>
+        </form> }
     </div>
   );
 }
